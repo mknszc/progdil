594b6c9b59172700cbcf92032fcf499d5a313cc9
mknszc <ozcan.camoglu@bil.omu.edu.tr>
2014-11-22 18:13:56 +0200

    2.soru ilk çözüm

---
+++ foo.swift
@@ -0,0 +1,40 @@
+func ReplaceUnderscoreWithSpace() -> String {
+    var startIndex = 0
+    var endIndex = 0
+    var array = [""]
+    var newString = ""
+    for character in myString {
+        array.append(String(character))
+        
+    }
+    for var index = 1; index < array.count; ++index {
+        if (array[index] != "_") {
+            startIndex = index
+            break
+            
+        }
+        
+    }
+    for var index = 0; index < array.count; ++index {
+        if (array[index] != "_") {
+            endIndex = index
+            
+        }
+        
+    }
+    for var index = startIndex; index < endIndex; ++index {
+        if (array[index] == "_") {
+            array[index] = " "
+            
+        }
+        
+    }
+    for var index = 0; index < array.count; ++index {
+        newString += array[index]
+        
+    }
+    return newString
+    
+}
+var myString = "foo_bar"
+ReplaceUnderscoreWithSpace()
