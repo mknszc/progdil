639ff97c334a716681143ed56003708cdaf7b2fd
Özcan ÇAMOĞLU <ozcan.camoglu@bil.omu.edu.tr>
2014-12-09 15:55:46 +0200

    sınıf metodu oluşturuldu

--- ReplaceUnderscoreWithSpace.swift
+++ ReplaceUnderscoreWithSpace.swift
@@ -1,29 +1,33 @@
-func ReplaceUnderscoreWithSpace(myString:String) -> String {
-    var startIndex = 0
-    var endIndex = 0
-    var array = Array(myString)
-    for var i = 0; i < array.count; ++i {
-        if (array[i] != "_") {
-            startIndex = i
-            break
-            
-        }
-    }
-    for var j = 0; j < array.count; ++j {
-        if (array[j] != "_") {
-            endIndex = j
-            }
-    }
-    for var k = startIndex; k < endIndex; ++k {
-        if (array[k] == "_") {
-            array[k] = " "
-        }
+class ReplaceUnderScoreWithSpace {
+    let myString: String
+    init(_ myString: String) {
+        self.myString = myString
         
     }
-    var newString = String(array)
-    return newString
+    func replaceUnderscoreWithSpace() -> String {
+        var startIndex = 0
+        var endIndex = 0
+        var array = Array(self.myString)
+        for var i = 0; i < array.count; ++i {
+            if (array[i] != "_") {
+                startIndex = i
+                break
+            }
+        }
+        for var j = 0; j < array.count; ++j {
+            if (array[j] != "_") {
+                endIndex = j
+            }
+        }
+        for var k = startIndex; k < endIndex; ++k {
+            if (array[k] == "_") {
+                array[k] = " "
+            }
+        }
+        return String(array)
+    }
 }
 
-println(ReplaceUnderscoreWithSpace("__foo_bar__"))
-            
+var newObject = ReplaceUnderScoreWithSpace("__foo__bar__")
+println(newObject.replaceUnderscoreWithSpace())
 
